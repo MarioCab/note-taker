f76ed7ce68c647a34bb3f027cfe385583c21fd8a
Mario Cabrera <yankeescab@gmail.com>
2021-05-03 18:22:55 -0400

    got api top read and write on json file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,33 @@
 const router = require("express").Router();
+const path = require("path");
+const fs = require("fs");
+const notes = require("../db/db.json");
+const uuid = require("uuid");
+const { json } = require("express");
+
 router.get("/notes", (req, res) => {
-  res.send("hello world");
+  // fs.readFile("./db/db.json", "utf8", (err, data) => {
+  //   if (err) throw err;
+  //   console.log(data);
+  res.json(notes);
+  // });
 });
+
+router.get("/notes/:id", (req, res) => {
+  res.json(notes.filter((note) => note.id === req.params.id));
+  // res.json(notes)
+});
+
+router.post("/notes", (req, res) => {
+  let newNote = req.body;
+  newNote.id = uuid.v1();
+  let updatedArray = notes;
+  updatedArray.push(newNote);
+
+  fs.writeFile("./db/db.json", JSON.stringify(updatedArray), (err, data) => {
+    if (err) throw err;
+    res.json(data);
+  });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use("/", htmlRoutes);
 app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
 });
